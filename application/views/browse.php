15b94ddef205f79d579e28bf973fd22b6b120ada
HasiniWij <61408819+HasiniWij@users.noreply.github.com>
2023-01-10 13:25:44 +0000

    refractor and format browse view

--- application/views/browse.php
+++ application/views/browse.php
@@ -1,43 +1,48 @@
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <title>Quizoo</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		
-		<style>
-			#categoryButton{
-				background-color: #4D47C3;
-				border: none;
-				color: white;
-				text-align: center;
-				text-decoration: none;
-				display: inline-block;
-				font-size: 14px;
-				cursor: pointer;
-				font-size: 30px;
-				width: 250px;
-				height: 130px;
-				margin: 30px;
-			}
-		</style>
-    </head>
-    <body>
+<html lang="en">
+
+<head>
+	<title>Quizoo</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<style>
+		#categoryButton {
+			background-color: #4D47C3;
+			border: none;
+			color: white;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 14px;
+			cursor: pointer;
+			font-size: 30px;
+			width: 250px;
+			height: 130px;
+			margin: 30px;
+		}
+
+		.titleStyle {
+			text-align: center;
+			padding-top: 5%
+		}
+	</style>
+</head>
+
+<body>
 	<div class="container" id="contentArea">
-		<div class='row' style="text-align: center; padding-top:5%">
-	       	<h1>Browse</h1>
+		<div class='row titleStyle'>
+			<h1>Browse</h1>
 		</div>
-		<div id="buttonArea"></div>	
+		<div id="buttonArea"></div>
 	</div>
-		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min.js"></script>
-		<script language="Javascript">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min.js"></script>
+
+	<script language="Javascript">
 		$(document).ready(function () {
 			var Category = Backbone.Model.extend({
-				// url: function () {s
-				// 	return "<?php echo base_url() ?>index.php/Celebs/celeb?name=";
-				// },
 				idAttribute: "category",
 				defaults: {
 					category: null
@@ -46,53 +51,42 @@
 
 			var Categories = Backbone.Collection.extend(
 				{
-					model : Category,
+					model: Category,
 					url: "<?php echo base_url() ?>index.php/QuizController/categories"
 				}
 			)
 
-			// create collections object
 			var categories = new Categories();
 
 			var ContentAreaView = Backbone.View.extend(
 				{
-					model: categories, // connect view to collections object
-					el : $('#contentArea'), // connect view to page area
-					events : {
-						"click #categoryButton" : "selectCategoryEvent"
+					model: categories,
+					el: $('#contentArea'),
+					events: {
+						"click #categoryButton": "selectCategoryEvent"
 					},
-					initialize : function () {
-						categories.fetch({async:false});
-						console.log(categories);
+					initialize: function () {
+						categories.fetch({ async: false });
 						this.render();
 					},
-					render : function () {
+					render: function () {
 						var self = this;
-							categories.each(function (c) {
-								console.log(c.get('category'));
-								var button = "<button type='button' id='categoryButton' data-category="+c.get('category')+">" + c.get('category')  + "</button>";
-								$( "#buttonArea" ).append(button);
-							})
+						categories.each(function (c) {
+							console.log(c.get('category'));
+							var button = "<button type='button' id='categoryButton' data-category=" + c.get('category') + ">" + c.get('category') + "</button>";
+							$("#buttonArea").append(button);
+						})
 					},
-					selectCategoryEvent : function (event) {
-						// console.log("ins")
+					selectCategoryEvent: function (event) {
 						var category = $(event.currentTarget).data('category');
-						// console.log(tagNumber)/
-						document.location.href = "<?php echo base_url()?>index.php/SearchQuizController/quizzesOfCategory/category/"+category;
-						// location.href='https://google.com';
-						// window.location.pathname = ('/newpage.html')
-						// $('.cname').remove()
-						// $(event.currentTarget).parent().append("<div class='cname'>"
-						// 	+ celebs.get(event.currentTarget.id).get('name') + " is <br/>"
-						// 	+ celebs.get(event.currentTarget.id).get('age') + " years old"
-						// 	+ "</div>")
+						document.location.href = "<?php echo base_url() ?>index.php/SearchQuizController/browseQuizzesView/category/" + category;
 					}
 				}
 			)
 
 			var contentArea = new ContentAreaView();
 		});
-		</script>
-			
-    </body>
-    </html>
+	</script>
+</body>
+
+</html>
